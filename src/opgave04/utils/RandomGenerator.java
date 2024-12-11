bd5d4c353109ddebc44543b6d8b711f1b6a980c8
benn-christensen <bnch@eaaa.dk>
2024-12-11 11:15:15 +0100

    Added StopWatch and RandonGenerator

--- src/opgave04/utils/RandomGenerator.java
+++ src/opgave04/utils/RandomGenerator.java
@@ -1,4 +1,14 @@
 package opgave04.utils;
 
+import java.util.Random;
+
 public class RandomGenerator {
+    public static int[] generateRandomArray(int size) {
+        int[] generatedNumbers = new int[size];
+        Random random = new Random();
+        for (int index = 0; index < size; index++) {
+            generatedNumbers[index] = random.nextInt();
+        }
+        return generatedNumbers;
+    }
 }
